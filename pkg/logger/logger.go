1a195ee1844b06faaeaa208d8e2854ba62db6861
vgjm <vgjm456@qq.com>
2022-03-24 15:32:14 +0800

    Fix: set up logger

--- pkg/logger/logger.go
+++ pkg/logger/logger.go
@@ -1,13 +1,46 @@
 package logger
 
 import (
+	"fmt"
 	"os"
 
 	"github.com/rs/zerolog"
-	"github.com/rs/zerolog/log"
+	"github.com/spf13/viper"
+	"golang.org/x/term"
 )
 
-// Init inits the logger
-func Init() {
-	log.Logger = log.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+func New() zerolog.Logger {
+	logger := zerolog.
+		New(os.Stderr).
+		With().
+		Timestamp().
+		Logger()
+
+	if !jsonLogs() {
+		logger = logger.Output(zerolog.ConsoleWriter{Out: os.Stderr})
+	} else {
+		logger = logger.With().Timestamp().Caller().Logger()
+	}
+
+	level := viper.GetString("log-level")
+	lvl, err := zerolog.ParseLevel(level)
+	if err != nil {
+		panic(err)
+	}
+	return logger.Level(lvl)
+}
+
+func jsonLogs() bool {
+	switch f := viper.GetString("log-format"); f {
+	case "json":
+		return true
+	case "plain":
+		return false
+	case "auto":
+		return !term.IsTerminal(int(os.Stdout.Fd()))
+	default:
+		fmt.Fprintf(os.Stderr, "invalid --log-format %q\n", f)
+		os.Exit(1)
+	}
+	return false
 }
