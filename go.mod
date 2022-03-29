c24ae8e7d06cdba4ec10f5b93510c0c27a967dbb
Jinming Guo <88078088+vgjm@users.noreply.github.com>
2022-03-29 09:19:43 +0800

    Feat: parse ./schemas/schema.yaml, support --set flag (#54)

--- go.mod
+++ go.mod
@@ -13,6 +13,8 @@ require (
 	github.com/spf13/cobra v1.3.0
 	github.com/spf13/viper v1.10.0
 	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211
+	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b
+	helm.sh/helm v2.17.0+incompatible
 )
 
 require (
@@ -23,6 +25,7 @@ require (
 	github.com/cockroachdb/apd/v2 v2.0.2 // indirect
 	github.com/emirpasic/gods v1.12.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
+	github.com/ghodss/yaml v1.0.0 // indirect
 	github.com/go-git/gcfg v1.5.0 // indirect
 	github.com/go-git/go-billy/v5 v5.0.0 // indirect
 	github.com/go-git/go-git/v5 v5.1.0 // indirect
@@ -82,6 +85,5 @@ require (
 	gopkg.in/ini.v1 v1.66.2 // indirect
 	gopkg.in/warnings.v0 v0.1.2 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
 	moul.io/http2curl v1.0.0 // indirect
 )
--- pkg/cmd/down.go
+++ pkg/cmd/down.go
@@ -2,27 +2,30 @@ package cmd
 
 import (
 	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
 
+	"github.com/h8r-dev/heighliner/pkg/logger"
 	"github.com/h8r-dev/heighliner/pkg/state"
 	"github.com/h8r-dev/heighliner/pkg/util"
 )
 
 var (
 	downCmd = &cobra.Command{
-		Use:                "down",
-		Short:              "Shut down the application and clear resources",
-		Args:               cobra.ArbitraryArgs,
-		DisableFlagParsing: true,
-		RunE:               downProj,
+		Use:   "down",
+		Short: "Shut down the application and clear resources",
+		Args:  cobra.ArbitraryArgs,
+		RunE:  downProj,
 	}
 )
 
 func downProj(c *cobra.Command, args []string) error {
+	lg := logger.New()
 	if err := state.EnterTemp(); err != nil {
-		return err
+		lg.Fatal().Err(err).Msg("Couldn't find project. Please run hln new to create one")
 	}
-	newArgs := make([]string, 0)
-	newArgs = append(newArgs, "do", "down", "-p", "./plans")
-	newArgs = append(newArgs, args...)
-	return util.Exec("dagger", newArgs...)
+	return util.Exec("dagger",
+		"--log-format", viper.GetString("log-format"),
+		"--log-level", viper.GetString("log-level"),
+		"-p", "./plans",
+		"do", "down")
 }
