003d25eed5e6fba8408c51d4145c29af42cefaab
vgjm <vgjm456@qq.com>
2022-04-09 09:41:26 +0800

    Fix: remove scripts
    Signed-off-by: vgjm <vgjm456@qq.com>

--- scripts/ci/install_staticcheck.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-set -ex
-
-if ! which staticcheck &> /dev/null
-then
-  echo 'installing honnef.co/go/tools/cmd/staticcheck...'
-  GO111MODULE=off go get honnef.co/go/tools/cmd/staticcheck 
-fi
