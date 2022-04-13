e56016a9569cb0665ebae6f9abcc3717e103753b
Hongchao Deng <hongchaodeng1@gmail.com>
2022-04-13 11:05:07 +0800

    feat: status cmd print more info (#94)

--- pkg/cmd/status.go
+++ pkg/cmd/status.go
@@ -18,29 +18,39 @@ import (
 	"github.com/h8r-dev/heighliner/pkg/util/k8sutil"
 )
 
+// statusOption controls the behavior of status command.
+type statusOption struct {
+	KubeconfigPath string
+	Namespace      string
+
+	Kubecli *kubernetes.Clientset
+}
+
 func newStatusCmd() *cobra.Command {
+	o := &statusOption{}
 	c := &cobra.Command{
 		Use:   "status",
 		Short: "Show status of your application",
 		Args:  cobra.NoArgs,
-		RunE:  getStatus,
+		RunE:  o.getStatus,
 	}
 
 	if home := homedir.HomeDir(); home != "" {
-		c.Flags().StringP("kubeconfig", "", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
+		c.Flags().StringVar(&o.KubeconfigPath, "", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
 	} else {
-		c.Flags().StringP("kubeconfig", "", "", "(optional) absolute path to the kubeconfig file")
+		c.Flags().StringVar(&o.KubeconfigPath, "", "", "(optional) absolute path to the kubeconfig file")
 	}
+	c.Flags().StringVar(&o.Namespace, "namespace", "default", "Specify the namespace")
 
 	return c
 }
 
-func getStatus(c *cobra.Command, args []string) error {
-	kubeconfigPath := c.Flags().Lookup("kubeconfig").Value.String()
-	kubecli, err := k8sutil.MakeKubeClient(kubeconfigPath)
+func (o *statusOption) getStatus(c *cobra.Command, args []string) error {
+	kubecli, err := k8sutil.MakeKubeClient(o.KubeconfigPath)
 	if err != nil {
 		return fmt.Errorf("failed to make kube client: %w", err)
 	}
+	o.Kubecli = kubecli
 
 	ao, err := loadAppOutput()
 	if err != nil {
@@ -53,30 +63,46 @@ func getStatus(c *cobra.Command, args []string) error {
 	// print app info
 	fmt.Fprintf(printTarget, "Application:\n")
 	fmt.Fprintf(printTarget, "  Name: %s\n", appName)
-	fmt.Fprintf(printTarget, "  Namespace: %s\n", ao.Application.Namespace)
+	fmt.Fprintf(printTarget, "  Namespace: %s\n", o.Namespace)
 	fmt.Fprintf(printTarget, "  Domain: %s\n", ao.Application.Domain)
 	fmt.Fprintf(printTarget, "  IP: %s\n", ao.Application.Ingress)
 
+	// print repos
+	fmt.Fprintf(printTarget, "Repositories:\n")
+	fmt.Fprintf(printTarget, "  Backend: %s\n", ao.Repository.Backend)
+	fmt.Fprintf(printTarget, "  Frontend: %s\n", ao.Repository.Frontend)
+	fmt.Fprintf(printTarget, "  Deploy: %s\n", ao.Repository.Deploy)
+
 	// print workload replicas and health
-	workload, err := loadWorkload(kubecli, appName, ao.Application.Namespace)
+	workload, err := o.loadWorkload(appName, o.Namespace)
 	if err != nil {
 		return fmt.Errorf("failed to load workload: %w", err)
 	}
 	fmt.Fprintf(printTarget, "Deployment:\n")
 	fmt.Fprintf(printTarget, "  Replicas: %d\n", workload.Spec.Replicas)
 
-	// print repos
+	for _, comp := range ao.Infra {
+		switch comp.Type {
+		case "argoCD":
+			fmt.Fprintf(printTarget, "ArgoCD:\n")
+			fmt.Fprintf(printTarget, "  Username: %s\n", comp.Username)
+			fmt.Fprintf(printTarget, "  Password: %s\n", comp.Password)
+		case "grafana":
+			fmt.Fprintf(printTarget, "Grafana:\n")
+			fmt.Fprintf(printTarget, "  Username: %s\n", comp.Username)
+			fmt.Fprintf(printTarget, "  Password: %s\n", comp.Password)
+		case "nocalhost":
+			fmt.Fprintf(printTarget, "Nocalhost:\n")
+			fmt.Fprintf(printTarget, "  Username: %s\n", comp.Username)
+			fmt.Fprintf(printTarget, "  Password: %s\n", comp.Password)
+		}
+	}
 
-	// print build pipelines and images
-
-	// print argocd app
-
-	// print db
 	return nil
 }
 
-func loadWorkload(kubecli *kubernetes.Clientset, name, ns string) (*appsv1.Deployment, error) {
-	deployment, err := kubecli.AppsV1().Deployments(ns).Get(context.TODO(), name, metav1.GetOptions{})
+func (o *statusOption) loadWorkload(name, ns string) (*appsv1.Deployment, error) {
+	deployment, err := o.Kubecli.AppsV1().Deployments(ns).Get(context.TODO(), name, metav1.GetOptions{})
 	if err != nil {
 		return nil, fmt.Errorf("failed to get deployment: %w", err)
 	}
