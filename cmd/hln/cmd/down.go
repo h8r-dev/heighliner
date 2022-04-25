0be34393e09d1bfc31fd0fa95e7271da206a18ef
vgjm <vgjm456@qq.com>
2022-04-25 16:56:38 +0800

    fix: downCmd delete argoCD
    Signed-off-by: vgjm <vgjm456@qq.com>

--- cmd/hln/cmd/down.go
+++ cmd/hln/cmd/down.go
@@ -1,148 +1,36 @@
 package cmd
 
 import (
-	"errors"
-	"os"
-	"path/filepath"
-	"strings"
+	"context"
 
-	"github.com/mitchellh/go-homedir"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
-	"github.com/spf13/viper"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
 
-	"github.com/h8r-dev/heighliner/pkg/dagger"
-	"github.com/h8r-dev/heighliner/pkg/project"
-	"github.com/h8r-dev/heighliner/pkg/schema"
-	"github.com/h8r-dev/heighliner/pkg/stack"
-	"github.com/h8r-dev/heighliner/pkg/state"
+	"github.com/h8r-dev/heighliner/pkg/kube"
 )
 
 // upOptions controls the behavior of up command.
 type downOptions struct {
-	Stack string
-	Dir   string
-	Local bool
-
-	Values []string
-
-	Interactive bool
-	NoCache     bool
+	Dir string
 
 	genericclioptions.IOStreams
 }
 
 func (o *downOptions) BindFlags(f *pflag.FlagSet) {
-	f.StringVarP(&o.Stack, "stack", "s", "", "Name of your stack")
 	f.StringVar(&o.Dir, "dir", "", "Path to your local stack")
-	f.StringArrayVar(&o.Values, "set", []string{}, "The input values of your project")
-	f.BoolVarP(&o.Interactive, "interactive", "i", false, "If this flag is set, heighliner will promt dialog when necessary.")
-	f.BoolVar(&o.NoCache, "no-cache", false, "Disable caching")
 }
 
 func (o *downOptions) Validate(cmd *cobra.Command, args []string) error {
-	if o.Stack != "" && o.Dir != "" {
-		return errors.New("please do not specify both stack and dir")
-	}
-	for _, v := range o.Values {
-		if !strings.Contains(v, "=") {
-			return errors.New("value format should be '--set key=value'")
-		}
-	}
-	pwd, err := os.Getwd()
-	if err != nil {
-		return err
-	}
-	if _, err := os.Stat(filepath.Join(pwd, appInfo)); err != nil {
-		return err
-	}
 	return nil
 }
 
 func (o *downOptions) Run() error {
-	if o.Dir != "" {
-		var err error
-		o.Dir, err = homedir.Expand(o.Dir)
-		if err != nil {
-			return err
-		}
-		o.Local = true
-	}
-	pwd, err := os.Getwd()
+	dclient, err := kube.NewDynamicClient()
 	if err != nil {
 		return err
 	}
-
-	// Prepare the satck
-	var p *project.Project
-	switch {
-	case o.Stack != "":
-		s, err := stack.New(o.Stack)
-		if err != nil {
-			return err
-		}
-		err = s.Update()
-		if err != nil {
-			return err
-		}
-		p = project.New(
-			filepath.Join(state.GetCache(), s.Name),
-			filepath.Join(state.GetTemp(), s.Name))
-
-	case o.Dir != "":
-		p = project.New(o.Dir, filepath.Join(state.GetTemp(), filepath.Base(o.Dir)))
-	default:
-		p = project.New(pwd, filepath.Join(state.GetTemp(), "hln"))
-	}
-
-	// Initialize the project.
-	// Enter the project dir automatically.
-	err = p.Init()
-	if err != nil {
-		return err
-	}
-
-	// Set input values.
-	for _, val := range o.Values {
-		envvar := strings.Split(val, "=")
-		envvar[1], err = homedir.Expand(envvar[1])
-		if err != nil {
-			return err
-		}
-		err := os.Setenv(envvar[0], envvar[1])
-		if err != nil {
-			return err
-		}
-	}
-	if o.Interactive {
-		sch := schema.New()
-		err = sch.AutomaticEnv(o.Interactive)
-		if err != nil {
-			return err
-		}
-	}
-
-	// Execute the action.
-	cli, err := dagger.NewClient(
-		viper.GetString("log-format"),
-		viper.GetString("log-level"),
-		o.IOStreams,
-	)
-	if err != nil {
-		return err
-	}
-	err = cli.Do(&dagger.ActionOptions{
-		Name:    "down",
-		Dir:     o.Dir,
-		Plan:    "./plans",
-		NoCache: o.NoCache,
-	})
-	if err != nil {
-		return err
-	}
-
-	return nil
+	return kube.DeleteArgoCDApps(context.Background(), dclient, "argocd", o.IOStreams)
 }
 
 func newDownCmd(streams genericclioptions.IOStreams) *cobra.Command {
