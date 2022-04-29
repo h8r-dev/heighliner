be8670d0b138ae14e9f2ab170567d6122555572f
vgjm <vgjm456@qq.com>
2022-04-29 11:37:25 +0800

    Fix: delete argocd apps according to output
    Signed-off-by: vgjm <vgjm456@qq.com>

--- cmd/hln/cmd/down.go
+++ cmd/hln/cmd/down.go
@@ -2,12 +2,20 @@ package cmd
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 	"github.com/spf13/pflag"
+	"go.uber.org/zap"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
+	"k8s.io/apimachinery/pkg/runtime/schema"
+	"k8s.io/apimachinery/pkg/types"
 	"k8s.io/cli-runtime/pkg/genericclioptions"
+	"k8s.io/client-go/dynamic"
 
-	"github.com/h8r-dev/heighliner/pkg/kube"
+	"github.com/h8r-dev/heighliner/pkg/logger"
+	"github.com/h8r-dev/heighliner/pkg/state/app"
 	"github.com/h8r-dev/heighliner/pkg/util/k8sutil"
 )
 
@@ -27,11 +35,18 @@ func (o *downOptions) Validate(cmd *cobra.Command, args []string) error {
 }
 
 func (o *downOptions) Run() error {
+	output, err := app.Load(appInfo)
+	if err != nil {
+		return err
+	}
 	dClient, err := k8sutil.NewFactory("").DynamicClient()
 	if err != nil {
 		return err
 	}
-	return kube.DeleteArgoCDApps(context.Background(), dClient, "argocd", o.IOStreams)
+	if err := deleteArgoCDApps(context.Background(), dClient, output.CD, o.IOStreams); err != nil {
+		return err
+	}
+	return nil
 }
 
 func newDownCmd(streams genericclioptions.IOStreams) *cobra.Command {
@@ -52,4 +67,36 @@ func newDownCmd(streams genericclioptions.IOStreams) *cobra.Command {
 	o.BindFlags(cmd.Flags())
 
 	return cmd
+}
+
+func deleteArgoCDApps(ctx context.Context,
+	dClient dynamic.Interface, cd app.CD,
+	streams genericclioptions.IOStreams) error {
+	lg := logger.New(streams)
+	for _, app := range cd.ApplicationRef {
+		if err := patchFinalizerAndDelete(ctx, dClient, cd.Namespace, app.Name, streams); err != nil {
+			lg.Info(color.HiYellowString("skip %s and continue", app.Name), zap.NamedError("warn", err))
+		}
+	}
+	return nil
+}
+
+func patchFinalizerAndDelete(ctx context.Context,
+	client dynamic.Interface,
+	namespace, name string,
+	streams genericclioptions.IOStreams) error {
+	const argoCDFinalizerRaw = `{"metadata": {"finalizers": ["resources-finalizer.argocd.argoproj.io"]}}`
+	lg := logger.New(streams)
+	var argoAppResource = schema.GroupVersionResource{
+		Group:    "argoproj.io",
+		Version:  "v1alpha1",
+		Resource: "applications",
+	}
+	argoApp := client.Resource(argoAppResource).Namespace(namespace)
+	_, err := argoApp.Patch(ctx, name, types.MergePatchType, []byte(argoCDFinalizerRaw), metav1.PatchOptions{})
+	if err != nil {
+		return err
+	}
+	lg.Info(fmt.Sprintf("patch finalizer to app %s", name))
+	return argoApp.Delete(ctx, name, metav1.DeleteOptions{})
 }
