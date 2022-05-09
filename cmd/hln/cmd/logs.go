de7a718efa319bee2ea194f180ce253f0d6f330c
Hongchao Deng <hongchaodeng1@gmail.com>
2022-05-08 21:10:14 -0400

    feat: select a service to print logs from (#163)
    * feat: select a service to print logs from
    
    * fix

--- cmd/hln/cmd/logs.go
+++ cmd/hln/cmd/logs.go
@@ -6,25 +6,24 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"path/filepath"
 
+	tea "github.com/charmbracelet/bubbletea"
 	"github.com/spf13/cobra"
-	v1 "k8s.io/api/core/v1"
+	corev1 "k8s.io/api/core/v1"
+	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 	"k8s.io/client-go/kubernetes"
 	"k8s.io/client-go/rest"
-	"k8s.io/client-go/util/homedir"
 
+	"github.com/h8r-dev/heighliner/pkg/state/app"
 	"github.com/h8r-dev/heighliner/pkg/util/k8sutil"
 )
 
 // LogsOptions controls the behavior of logs command.
 type LogsOptions struct {
-	Namespace string
-	Pod       string
+	Choice int
 
 	// PodLogOptions
-	Follow    bool
-	Container string
+	Follow bool
 
 	Kubecli *kubernetes.Clientset
 }
@@ -33,24 +32,9 @@ func newLogsCmd() *cobra.Command {
 	o := &LogsOptions{}
 
 	cmd := &cobra.Command{
-		Use:   "logs [POD]",
-		Short: "Print the logs for a container in a pod",
-		Args:  cobra.MinimumNArgs(1),
-		RunE: func(cmd *cobra.Command, args []string) error {
-			var err error
-			kubeconfigPath := cmd.Flags().Lookup("kubeconfig").Value.String()
-			o.Kubecli, err = k8sutil.NewFactory(kubeconfigPath).KubernetesClientSet()
-			if err != nil {
-				return fmt.Errorf("failed to make kube client: %w", err)
-			}
-			o.Pod = args[0]
-			return o.getPodLogs()
-		},
-	}
-	if home := homedir.HomeDir(); home != "" {
-		cmd.Flags().StringP("kubeconfig", "", filepath.Join(home, ".kube", "config"), "(optional) absolute path to the kubeconfig file")
-	} else {
-		cmd.Flags().StringP("kubeconfig", "", "", "(optional) absolute path to the kubeconfig file")
+		Use:   "logs",
+		Short: "Print the logs for an app",
+		RunE:  o.getPodLogs,
 	}
 	o.addFlags(cmd)
 	return cmd
@@ -58,14 +42,57 @@ func newLogsCmd() *cobra.Command {
 
 func (o *LogsOptions) addFlags(cmd *cobra.Command) {
 	cmd.Flags().BoolVarP(&o.Follow, "follow", "f", o.Follow, "Specify if the logs should be streamed.")
-	cmd.Flags().StringVarP(&o.Container, "container", "c", o.Container, "Print the logs of this container")
-	cmd.Flags().StringVar(&o.Namespace, "namespace", "default", "Specify the namespace")
 }
 
-func (o *LogsOptions) getPodLogs() error {
-	request := o.Kubecli.CoreV1().Pods(o.Namespace).GetLogs(o.Pod, &v1.PodLogOptions{
-		Container: o.Container,
-		Follow:    o.Follow,
+func getServiceNames(services []app.Service) []string {
+	var names []string
+	for _, s := range services {
+		names = append(names, s.Name)
+	}
+	return names
+}
+
+func (o *LogsOptions) getPodLogs(cmd *cobra.Command, args []string) error {
+	fact := k8sutil.NewFactory(k8sutil.GetKubeConfigPath())
+	k8sClient, err := fact.KubernetesClientSet()
+	if err != nil {
+		return err
+
+	}
+	o.Kubecli = k8sClient
+
+	appInfo, err := app.Load(appInfo)
+	if err != nil {
+		return err
+	}
+	names := getServiceNames(appInfo.Services)
+
+	// ask user to select one of the services to get logs from
+	p := tea.NewProgram(initialModel(names, &o.Choice))
+	if err := p.Start(); err != nil {
+		return err
+	}
+
+	namespace := fmt.Sprintf("%s-deploy-production", appInfo.ApplicationRef.Name)
+	svc, err := o.Kubecli.CoreV1().Services(namespace).Get(context.TODO(), names[o.Choice], metav1.GetOptions{})
+	if err != nil {
+		return err
+	}
+
+	podlist, err := o.Kubecli.CoreV1().Pods(namespace).List(context.TODO(), metav1.ListOptions{
+		LabelSelector: metav1.FormatLabelSelector(&metav1.LabelSelector{
+			MatchLabels: svc.Spec.Selector,
+		}),
+	})
+	if err != nil {
+		return err
+	}
+	if len(podlist.Items) == 0 {
+		return fmt.Errorf("no pods found for service %s", names[o.Choice])
+	}
+
+	request := o.Kubecli.CoreV1().Pods(namespace).GetLogs(names[o.Choice], &corev1.PodLogOptions{
+		Follow: o.Follow,
 	})
 	return DefaultConsumeRequest(request, os.Stdout)
 }
@@ -99,4 +126,84 @@ func DefaultConsumeRequest(request rest.ResponseWrapper, out io.Writer) error {
 			return nil
 		}
 	}
+}
+
+type model struct {
+	choices   []string // items on the to-do list
+	cursor    int      // which to-do list item our cursor is pointing at
+	choiceRef *int
+}
+
+func initialModel(choices []string, choiceRef *int) model {
+	return model{
+		choices:   choices,
+		choiceRef: choiceRef,
+	}
+}
+
+func (m model) Init() tea.Cmd {
+	return nil
+}
+
+func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
+	// nolint
+	switch msg := msg.(type) {
+
+	// Is it a key press?
+	case tea.KeyMsg:
+
+		// Cool, what was the actual key pressed?
+		switch msg.String() {
+
+		// These keys should exit the program.
+		case "ctrl+c", "q":
+			return m, tea.Quit
+
+		// The "up" and "k" keys move the cursor up
+		case "up", "k":
+			if m.cursor > 0 {
+				m.cursor--
+			}
+
+		// The "down" and "j" keys move the cursor down
+		case "down", "j":
+			if m.cursor < len(m.choices)-1 {
+				m.cursor++
+			}
+
+		// The "enter" key and the spacebar (a literal space) toggle
+		// the selected state for the item that the cursor is pointing at.
+		case "enter", " ":
+			*m.choiceRef = m.cursor
+			return m, tea.Quit
+		}
+	}
+
+	// Return the updated model to the Bubble Tea runtime for processing.
+	// Note that we're not returning a command.
+	return m, nil
+}
+
+func (m model) View() string {
+	// The header
+	s := "Select a service to get logs from\n\n"
+
+	// Iterate over our choices
+	for i, choice := range m.choices {
+
+		// Is the cursor pointing at this choice?
+		cursor := " " // no cursor
+		if m.cursor == i {
+			cursor = ">" // cursor!
+		}
+
+		// Render the row
+		s += fmt.Sprintf("%s %s\n", cursor, choice)
+	}
+
+	// The footer
+	s += "\nPress q to quit.\n"
+
+	// Send the UI for rendering
+	return s
 }
